ba29507dd42bfb960ec9e5cbc0c2dd870ffe11d4
adityagurav40 <113020833+adityagurav40@users.noreply.github.com>
2023-04-26 15:11:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+ const evaluatedText = document.getElementById("evaluatedText");
+    const letterCount = document.getElementById("letterCount");
+    
+    evaluatedText.addEventListener("input", () => {
+      const text = evaluatedText.value;
+      const count = text.length;
+      letterCount.innerText = count;
+    });
